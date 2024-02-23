bda74ee1dc15d47104cbefa633beea299fa94063
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2024-02-23 12:04:09 -0600

    after trial and error, i got it right

--- django/313-Django/first_project/first_project/urls.py
+++ django/313-Django/first_project/first_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
+from django.conf.urls import include
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('first_app/', include('first_app.urls')),  
     path('admin/', admin.site.urls),
 ]
