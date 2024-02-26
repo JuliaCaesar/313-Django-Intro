391496a06844ca2404591be9272f119cf90cf2ee
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2024-02-26 15:21:55 -0600

    final changes in class

--- django/313-Django/first_project/first_app/views.py
+++ django/313-Django/first_project/first_app/views.py
@@ -13,4 +13,10 @@ def index(request):
     return render(request, 'first_app/index.html', context=dict)
 
 def about(request):
-    return HttpResponse('<h1>This is the About Page</h1>') 
+    dict = {
+        "insert_me" : "This is from views.py", 
+        "fname" : "Rose",
+        "lname" : "Tyler",
+        "age" : "sad"
+    }
+    return render(request, 'first_app/about.html', context=dict) 
